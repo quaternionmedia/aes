d602645755ff6d57c94cf9c289d0a505ce1d69e2
Harpo <harpo@harpo.me>
2021-08-05 19:49:04 -0700

    working send / recv scripts!!!

---
+++ recv.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sudo ./mai -i eno1 -m recv -a 239.69.203.112 -s sess2 -t AOIP22 -b 24 -r 48000 -c 2 -p 250 -v -u 1000 -g 0
---
+++ send.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sudo ./mai -i eno1 -m send -a 239.69.69.99 -s sess1 -b 24 -r 48000 -c 2 -p 250 -v -u 1000 -g 0
